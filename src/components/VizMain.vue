60a0445b983d61b25e1ec066732cc0634ec19827
Michael Burak <michaelrburak@gmail.com>
2018-07-27 15:35:59 -0400

    main div in app is now VizMain component with tab viz

---
+++ src/components/VizMain.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="VizMain">
+    <Tableau 
+    url="https://public.tableau.com/views/Unemployment-MRB-72618/Sheet1"
+    height="500px" 
+    width="500px" 
+    ref="tableau">
+</Tableau>
+</div>
+</template>
+
+<script>
+export default {
+  name: "VizMain"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.VizMain {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  visibility: hidden;
+}
+</style>
